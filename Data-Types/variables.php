b6f448be3274eaa07639e2aee48bb5a5b06191bc
Abhishek <abhishekbhikule76@gmail.com>
2024-06-26 15:28:49 +0530

    Variables added

---
+++ Data-Types/variables.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Variables</title>
+    </head>
+    <body>
+        <?php 
+        $my_Name = "Hello I am learning Varialbles";
+        echo $my_Name . "<br/> ";
+        $var1 = 10;
+        $var2 = 20;
+        $var3 = $var1 + $var2;
+        echo "This is the total " . $var3;
+        echo "<br>";
+        $var4 = 40;
+        echo "This is the new total: " . $var1 + $var4;
+
+        ?>
+    </body>
+</html>
