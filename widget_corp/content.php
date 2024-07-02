5665c4ebe9ebd32190557a7fc2fbcc1f08060b6d
Abhishek <abhishekbhikule76@gmail.com>
2024-07-02 11:00:48 +0530

    learned how to do create in CRUD and Insert cmd in SQL

--- widget_corp/content.php
+++ widget_corp/content.php
@@ -1,60 +1,17 @@
 
 <?php require_once("includes/connection.php") ?>
 <?php require_once("includes/functions.php") ?>
-<?php 
-    if(isset($_GET['subj'])){
-        $sel_subject = get_subjects_by_id($_GET['subj']);
-        $sel_page = null;
-    }
-    elseif(isset($_GET['page'])){
-        $sel_subject = null;
-        $sel_page = get_pages_by_id($_GET['page']);
-    }
-    else{
-        $sel_subject = null; 
-        $sel_page = null;
-    }
-?>
+<?php find_selected_page() ?>
+
 
 <?php include("includes/header.php"); ?>
 
 <table id="structure">
 	<tr>
-		<td id="navigation">
-            <ul class="subjects">
-                <!-- // Setp3. Perform Data Query -->
-                <?php
-                $subject_set = get_all_subjects();
-
-                //setup 4. Return data
-                while($subject = mysqli_fetch_array($subject_set)){
-                    echo "<li";
-                    // I added isset() checks to ensure that the $sel_subject and $sel_page variables are set before accessing their properties. This should prevent the error message from appearing.
-                    if(isset($sel_subject) && $subject["id"] == $sel_subject["id"]){
-                        echo " class=\"selected\" ";
-                    }
-                    echo  "> <a href=\"content.php?subj=" . urlencode($subject["id"]) .
-                    "\" >  {$subject["menu_name"]} </a> </li>  ";
-                    
-
-                    $page_set = get_pages_for_subject($subject["id"]);
-
-                    //setup 4. Return data
-                    echo "<ul class=\"pages\">";
-                    while($page = mysqli_fetch_array($page_set)){
-                        echo "<li";
-                        //I added isset() checks to ensure that the $sel_subject and $sel_page variables are set before accessing their properties. This should prevent the error message from appearing.
-                        if(isset($sel_page) && $page["id"] == $sel_page["id"]){
-                            echo " class=\"selected\" ";
-                        }
-                        echo "> <a href=\"content.php?page=" . urlencode($page["id"]) .
-                        "\" >  {$page["menu_name"]} </a> </li>";
-                    }
-                    echo "</ul>";
-                }
-
-                ?>		
-            </ul>
+    <td id="navigation">
+           <?php echo navigation($sel_subject, $sel_page); ?>
+            <br>
+            <a href="new_subject.php">+ Add a new subject</a>
         </td>
 		<td id="page">
         
