a22bf5f848f00a590aeb3e6b252e9ff1c0704e27
Abhishek <abhishekbhikule76@gmail.com>
2024-07-03 13:36:00 +0530

    Encapsulation, Getter&Setter, Class

---
+++ OOP/Encapsulation_gettersetter.php
@@ -0,0 +1,33 @@
+<?php
+
+class Person {
+
+    private $name; // Encapsulation
+
+    private $age; // Encapsulation
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    function getAge(){ // Getters
+        return $this->age * 10;
+    }
+
+    function setAge($age){ // Setters
+        if($age < 18){
+            throw new Exception("Age should be greater than or equal to 18");
+        }
+
+        $this->age = $age;
+    }
+
+
+}
+
+$abhishek = new Person("Abhishek");
+$abhishek->setAge(20);
+var_dump($abhishek);
+
+?>
